f6e1ef21e31b46520ce81bde30cd03dee543a3de
EliBrito <elileite9@gmail.com>
2025-05-15 17:52:03 -0300

    feature/dev: Implementando novos tratamentos de erros.

--- src/main/java/br/com/fiap/locatech/locatech/controllers/AluguelController.java
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/AluguelController.java
@@ -3,6 +3,7 @@ package br.com.fiap.locatech.locatech.controllers;
 import br.com.fiap.locatech.locatech.dtos.AluguelRequestDTO;
 import br.com.fiap.locatech.locatech.entities.Aluguel;
 import br.com.fiap.locatech.locatech.services.AluguelService;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -53,7 +54,7 @@ public class AluguelController {
 
     @PostMapping
     public ResponseEntity<Void> saveAluguel(
-            @RequestBody AluguelRequestDTO aluguel
+            @Valid @RequestBody AluguelRequestDTO aluguel
     ) {
         logger.info("POST -> /alugueis");
         aluguelService.saveAluguel(aluguel);
